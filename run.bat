03d6fc0d745fc70cbd7f7da814db0c785bef5d2d
haisenzhao <haisenzhao@gmail.com>
2020-06-12 20:45:24 -0700

    update

---
+++ run.bat
@@ -0,0 +1,86 @@
+ECHO ON
+
+cd build/Release
+
+rem SmartCFS.exe ..\..\workspace\fertility\volume_1_100\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_2_r100_1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_2_r100_2\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_2_r100_3\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_3_r0d5_0d7_r0d5\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_4_0d7_0_0d7\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_5_r0d7_0_r0d7\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_6_0d5_0d7_0d5\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\fertility\volume_7_0_0d7_0d7\ 2.0 0.9 1 0 0
+
+
+rem SmartCFS.exe ..\..\workspace\bunny\volume_1_00r1\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\bunny\volume_2_001\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\bunny\volume_3_010\ 2.0 0.9 0 0 0
+
+
+rem SmartCFS.exe ..\..\workspace\kitten\volume_1_00r1\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\kitten\volume_2_001_1\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\kitten\volume_2_001_2\ 2.0 0.9 0 0 0
+
+rem SmartCFS.exe ..\..\workspace\biama\volume_1_00r1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\biama\volume_2_001\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\biama\volume_3_r100\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\biama\volume_4_100\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\screw\volume_1_00r1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\screw\volume_2_001_1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\screw\volume_2_001_2\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\screw\volume_2_001_3\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\rocker-arm\volume_1_00r1_1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\rocker-arm\volume_1_00r1_2\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\rocker-arm\volume_2_001_1\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\Fig\bunny_volume0\ 2.0 8.0 1 1 0
+
+rem SmartCFS.exe ..\..\workspace\dent\volume_0_010\ 2.0 0.3 1 1 0
+rem SmartCFS.exe ..\..\workspace\dent\volume_0_01r1\ 2.0 0.3 1 1 0
+rem SmartCFS.exe ..\..\workspace\dent\volume_1_010\ 2.0 0.3 1 1 0
+rem SmartCFS.exe ..\..\workspace\dent\volume_1_01r1\ 2.0 0.3 1 1 0
+rem SmartCFS.exe ..\..\workspace\dent\volume_2_010\ 2.0 0.3 1 1 0
+rem SmartCFS.exe ..\..\workspace\dent\volume_2_01r1\ 2.0 0.3 1 1 0
+
+rem SmartCFS.exe ..\..\workspace\max_planck\volume_1_r100\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\max_planck\volume_2_100\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\max_planck\volume_3_001\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\pony\volume_1_100_1\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\pony\volume_1_100_2\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\pony\volume_2_r100_1\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\pony\volume_2_r100_2\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\pony\volume_3_001_1\ 2.0 0.9 0 0 0
+rem SmartCFS.exe ..\..\workspace\pony\volume_4_00r1_1\ 2.0 0.9 0 0 0
+
+rem SmartCFS.exe ..\..\workspace\pawn\pawn_volume_1_r100_1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\pawn\pawn_volume_2_100_1\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\horse\volume_1_r100_1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\horse\volume_1_r100_2\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\horse\volume_2_100_1\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\horse\volume_2_100_2\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\horse\volume_3_001_1\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_1_100\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_2_001\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_3_r0.50.50.5\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_4_0.50.7r0.5\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_5_0.50.70.5\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_6_r0.50.7r0.5\ 2.0 0.9 1 0 0
+rem SmartCFS.exe ..\..\workspace\genus_box\volume_7_0.70.70\ 2.0 0.9 1 0 0
+
+rem SmartCFS.exe ..\..\workspace\cage\volume_0\ 2.0 0.9 1 0 0 1
+rem SmartCFS.exe ..\..\workspace\cage\volume_1\ 2.0 0.9 1 0 0 2
+rem SmartCFS.exe ..\..\workspace\cage\volume_2\ 2.0 0.9 1 1 1 3
+rem SmartCFS.exe ..\..\workspace\cage\volume_3\ 2.0 0.9 1 0 0 4
+rem SmartCFS.exe ..\..\workspace\cage\volume_4\ 2.0 0.9 1 0 0 5
+rem SmartCFS.exe ..\..\workspace\cage\volume_5\ 2.0 0.9 1 0 0 6
+rem SmartCFS.exe ..\..\workspace\cage\volume_6\ 2.0 0.9 1 0 0 7
+rem SmartCFS.exe ..\..\workspace\cage\volume_7\ 2.0 0.9 1 0 0 8
+rem SmartCFS.exe ..\..\workspace\cage\volume_8\ 2.0 0.9 1 0 0 9
+SmartCFS.exe ..\..\workspace\cage\volume_9\ 2.0 0.9 1 0 0 10
+
